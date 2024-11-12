dcf4079b86b6187c3ffcc26f58d0098bc35f0bbc
Adri K. Grow <42044618+adriannagrow@users.noreply.github.com>
2024-11-11 21:19:16 -0500

    Update wrapper_submit.sh
    Updated modules for current grid use.

--- PTL1/Transcriptomes/Scripts/wrapper_submit.sh
+++ PTL1/Transcriptomes/Scripts/wrapper_submit.sh
@@ -13,16 +13,16 @@ module purge       #Cleans up any loaded modules
 module use /gridapps/modules/all    #make sure module locations is loaded
 
 module load slurm
-module load tqdm
-module load Biopython/1.75-foss-2019b-Python-3.7.4
-module load BLAST+/2.9.0-gompi-2019b
-module load DIAMOND/0.9.30-GCC-8.3.0
-module load VSEARCH/2.21.1-GCC-10.3.0
+module load tqdm/4.62.3-GCCcore-11.2.0
+module load Biopython/1.79-foss-2021b
+module load BLAST+/2.12.0-gompi-2021b
+module load DIAMOND/2.0.13-GCC-11.2.0
+module load VSEARCH/2.22.1-GCC-11.3.0
 
 parent='/beegfs/fast/katzlab/becky/PTL1/Transcriptomes/Forams/'
 
 srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py -1 1 -2 7 -x --assembled_transcripts ${parent}AssembledTranscripts -o ${parent}  -n ${parent}Conspecific.txt --genetic_code Universal &
-#srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 2 -2 7 -x --assembled_transcripts ${parent}Plate4/Assembled_Transcripts -o ${parent}Plate4  -n ${parent}Plate4/Conspecific.txt --genetic_code ${parent}Plate4/Gcodes.txt &
+#srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py -1 1 -2 7 -x --assembled_transcripts ${parent}Assembled_Transcripts -o ${parent}  -n ${parent}Conspecific.txt --genetic_code ${parent}Gcodes.txt > log.out &
 #srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 2 -2 7 -x --assembled_transcripts ${parent}Plate7/Assembled_Transcripts -o ${parent}Plate7  -n ${parent}Plate7/Conspecific.txt --genetic_code ${parent}Plate7/Gcodes.txt &
 #srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 1 -2 7 -x --assembled_transcripts ${parent}Plate11/Assembled_Transcripts -o ${parent}Plate11  -n ${parent}Plate11/Conspecific.txt --genetic_code ${parent}Plate11/Gcodes.txt &
 #srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 2 -2 7 -x --assembled_transcripts ${parent}Plate18/Assembled_Transcripts -o ${parent}Plate18  -n ${parent}Plate18/Conspecific.txt --genetic_code ${parent}Plate18/Gcodes.txt &
