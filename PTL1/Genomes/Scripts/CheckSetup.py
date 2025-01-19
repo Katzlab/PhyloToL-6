f5bb221378aac9769865b84190f4ab0c435a0a79
Auden Cote-L'Heureux <52716489+AudenCote@users.noreply.github.com>
2025-01-19 10:54:49 -0500

    Updating header in CheckSetup.py

--- PTL1/Genomes/Scripts/CheckSetup.py
+++ PTL1/Genomes/Scripts/CheckSetup.py
@@ -1,7 +1,7 @@
 # Last updated Nov 2023
 # Author: Auden Cote-L'Heureux
 
-# This script is run as the first step of the PhyloToL 6 Part 1 GENOMES pipeline,
+# This script is run as the first step of the EukPhylo Part 1 GENOMES pipeline,
 # before any sequence data are actually processed. It checks to ensure that the input
 # CDS files and databases are properly located and formatted.
 
