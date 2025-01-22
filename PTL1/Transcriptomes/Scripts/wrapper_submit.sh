5284a71ce8ecce54dace98e0d0af201a8d113ed2
Auden Cote-L'Heureux <52716489+AudenCote@users.noreply.github.com>
2025-01-22 12:56:11 -0500

    Updating headers and example commands in wrapper_submit.sh

--- PTL1/Transcriptomes/Scripts/wrapper_submit.sh
+++ PTL1/Transcriptomes/Scripts/wrapper_submit.sh
@@ -1,17 +1,23 @@
+## Last updated Jan 2025 by Auden Cote-L'Heureux
+
+## This script is intended to be used to process genomic CDS with EukPhylo part 1 on an HPC that uses the Slurm workload manager.
+## The first part of the script are Slurm-specific parameters that should be adjusted by users to fit their resource allocation
+## needs and restrictions, followed by some example commands taken from the GitHub Wiki, more detail for which can be found
+## here: https://github.com/Katzlab/EukPhylo/wiki/EukPhylo-Part-1:-GF-assignment
+
+
 #!/bin/bash
-#
-#SBATCH --job-name=PTL1_GBF
-#SBATCH --output=PTL1.%j.out # Stdout (%j expands to jobId)
+
+## Slurm specific code
+
+#SBATCH --job-name=EukPhylo
+#SBATCH --output=EukPhylo.%j.out # Stdout (%j expands to jobId)
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=64 ##change to number of srun when running multiple instances
+#SBATCH --ntasks-per-node=64 # #change to number of srun when running multiple instances
 #SBATCH --mem=160G
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=YOUREMAIL@smith.edu
 
 module purge       #Cleans up any loaded modules
-module use /gridapps/modules/all    #make sure module locations is loaded
-
 module load slurm
 module load tqdm/4.62.3-GCCcore-11.2.0
 module load Biopython/1.79-foss-2021b
@@ -19,11 +25,12 @@ module load BLAST+/2.12.0-gompi-2021b
 module load DIAMOND/2.0.13-GCC-11.2.0
 module load VSEARCH/2.22.1-GCC-11.3.0
 
-parent='/beegfs/fast/katzlab/becky/PTL1/Transcriptomes/Forams/'
+parent='/Your/Home/Folder'
 
-srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py -1 1 -2 7 -x --assembled_transcripts ${parent}AssembledTranscripts -o ${parent}  -n ${parent}Conspecific.txt --genetic_code Universal &
-#srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py -1 1 -2 7 -x --assembled_transcripts ${parent}Assembled_Transcripts -o ${parent}  -n ${parent}Conspecific.txt --genetic_code ${parent}Gcodes.txt > log.out &
-#srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 2 -2 7 -x --assembled_transcripts ${parent}Plate7/Assembled_Transcripts -o ${parent}Plate7  -n ${parent}Plate7/Conspecific.txt --genetic_code ${parent}Plate7/Gcodes.txt &
-#srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 1 -2 7 -x --assembled_transcripts ${parent}Plate11/Assembled_Transcripts -o ${parent}Plate11  -n ${parent}Plate11/Conspecific.txt --genetic_code ${parent}Plate11/Gcodes.txt &
-#srun -D ${parent}HQ/Scripts python3 ${parent}HQ/Scripts/wrapper.py -1 2 -2 7 -x --assembled_transcripts ${parent}Plate18/Assembled_Transcripts -o ${parent}Plate18  -n ${parent}Plate18/Conspecific.txt --genetic_code ${parent}Plate18/Gcodes.txt &
-wait
+## Example commands
+
+# A simple run that goes from script 1 to script 7 (the last script) using the Universal genetic code
+srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py --first_script 1 --last_script 7 --assembled_transcripts ${parent}AssembledTranscripts --genetic_code ${parent}Gcode.txt --databases ${parent}Databases > log.txt
+
+# Including the cross-plate contamination step, using conspecific names
+srun -D ${parent} python3 ${parent}Scripts/wrapper.py --first_script 1 --last_script 7 --assembled_transcripts ${parent}AssembledTranscripts --output . --genetic_code ${parent}Gcode.txt --databases ${parent}Databases --xplate_contam --conspecific_names ${parent}Conspecific.txt > log.txt
