4f033e8ab299341a21a55c3e22d06206d5592130
Auden Cote-L'Heureux <52716489+AudenCote@users.noreply.github.com>
2025-01-22 12:51:10 -0500

    Updating header and example runs in wrapper_submit.sh

--- PTL1/Genomes/Scripts/wrapper_submit.sh
+++ PTL1/Genomes/Scripts/wrapper_submit.sh
@@ -1,24 +1,33 @@
+## Last updated Jan 2025 by Auden Cote-L'Heureux
+
+## This script is intended to be used to process genomic CDS with EukPhylo part 1 on an HPC that uses the Slurm workload manager.
+## The first part of the script are Slurm-specific parameters that should be adjusted by users to fit their resource allocation
+## needs and restrictions, followed by some example commands taken from the GitHub Wiki, more detail for which can be found
+## here: https://github.com/Katzlab/EukPhylo/wiki/EukPhylo-Part-1:-GF-assignment
+
+
 #!/bin/bash
-#
-#SBATCH --job-name=PTL1_genome
-#SBATCH --output=PTL1.%j.out # Stdout (%j expands to jobId)
+
+## Slurm specific code
+
+#SBATCH --job-name=EukPhylo
+#SBATCH --output=EukPhylo.%j.out # Stdout (%j expands to jobId)
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=64 ##change to number of srun when running multiple instances
+#SBATCH --ntasks-per-node=64 # #change to number of srun when running multiple instances
 #SBATCH --mem=160G
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=YOUREMAIL@smith.edu
 
 module purge       #Cleans up any loaded modules
-module use /gridapps/modules/all    #make sure module locations is loaded
-
 module load slurm
 module load tqdm
 module load Biopython/1.75-foss-2019b-Python-3.7.4
 module load BLAST+/2.9.0-gompi-2019b
 module load DIAMOND/0.9.30-GCC-8.3.0
 
-path='/beegfs/fast/katzlab/PTL1/Genomes/'
+path='/Your/Home/Folder'
 
-srun -D ${path}Scripts python3 ${path}Scripts/wrapper.py -1 1 -2 5 --cds ${path}PTL1GenomesBatches/PTL1GenomesBatch2 -o ${path}Output/PTL1Genomes_OutputBatch2 --genetic_code Universal --databases ${path}Databases &
-wait
+## Example run command
+
+# Start at script 1 and go through script 5 (the final script) using the Universal genetic code
+srun -D ${path}Scripts python3 ${path}Scripts/wrapper.py -1 1 -2 5 --cds ${path}Input -o ${path}Output --genetic_code Universal --databases ${path}Databases 
+
